436ff971761adf74faf6e25d992a1b6236a04f4d
Ilja <iljapanic@gmail.com>
2016-09-08 13:00:32 +0200

    js for opening external links in new tab

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){});
+$(document).ready(function(){$('article a[href^="http://"]').not("a[href*=takemetodenmark]").attr("target","_blank"),$('article a[href^="http://"]').not("a[href*=localhost]").attr("target","_blank"),$('article a[href^="https://"]').not("a[href*=takemetodenmark]").attr("target","_blank"),$('article a[href^="https://"]').not("a[href*=localhost]").attr("target","_blank")});
