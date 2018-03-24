58574e034e26870136e04009850c50536510e455
Ilja <iljapanic@gmail.com>
2018-03-24 21:54:06 +0000

    gulp task refactoring

--- gulpfile.js/tasks/clean.js
+++ gulpfile.js/tasks/clean.js
@@ -2,9 +2,12 @@ var config = require('../config')
 
 var gulp = require('gulp')
 var del = require('del')
+var path = require('path')
+
+var markdownFiles = path.join(config.root.src, config.tasks.html.src, config.tasks.markdown.dest)
 
 var cleanTask = function (cb) {
-  del([config.root.dest]).then(function (paths) {
+  del([config.root.dest, markdownFiles]).then(function (paths) {
     cb()
   })
 }
